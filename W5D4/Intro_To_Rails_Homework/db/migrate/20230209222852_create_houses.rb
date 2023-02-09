9911c81b58d2fb2881d2d25162241f82f25a53b4
Steven Notovitz <sjn39@cornell.edu>
2023-02-09 17:39:53 -0500

    .

--- W5D4/Intro_To_Rails_Homework/db/migrate/20230209222852_create_houses.rb
+++ W5D4/Intro_To_Rails_Homework/db/migrate/20230209222852_create_houses.rb
@@ -1,7 +1,7 @@
 class CreateHouses < ActiveRecord::Migration[7.0]
   def change
     create_table :houses do |t|
-      t.string, :address, null: false
+      t.string :address, null: false
       t.timestamps
     end
 
--- W5D4/Intro_To_Rails_Homework/db/schema.rb
+++ W5D4/Intro_To_Rails_Homework/db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_09_220810) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_09_222852) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "houses", force: :cascade do |t|
+    t.string "address", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["address"], name: "index_houses_on_address", unique: true
+  end
 
   create_table "people", force: :cascade do |t|
     t.string "name", null: false
