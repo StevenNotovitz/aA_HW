ecc1eb23edd1d5c110a6b468527fa9dd59959d47
Steven Notovitz <sjn39@cornell.edu>
2023-02-28 10:51:50 -0500

    Add w8d3

--- W8D3/Intro JavaScript Problems (Part 2).js
+++ W8D3/Intro JavaScript Problems (Part 2).js
@@ -1,9 +1,13 @@
 function titleize(names, callback) {
-    let titleized = names.map(name => `Mx. ${name} Jingleheimer Schmidt`)
-    callback(titleized)
+    const titles = names.map((name) => {
+        `Mx. ${name} Jingleheimer Schmidt`
+    })
+    callback(titles)
 }
-const printCallback = function(names) {
-    names.forEach( name => {console.log(name)} );
+const printCallback = function(titles) {
+    titles.forEach((title) => {
+        console.log(title)
+    })
 }
 titleize(["Mary", "Brian", "Leo"], printCallback)
 
@@ -12,25 +16,25 @@ function Elephant(name, height, tricks) {
     this.height = height
     this.tricks = tricks
 }
-Elephant.prototype.trumpet = function () {
+Elephant.prototype.trumpet = function() {
     console.log(`${this.name} the elephant goes 'phrRRRRRRRRRRR!!!!!!!'`)
 }
-Elephant.prototype.grow = function () {
+Elephant.prototype.grow = function() {
     this.height += 12
 }
-Elephant.prototype.addTrick = function (trick) {
+Elephant.prototype.addTrick = function(trick) {
     this.tricks.push(trick)
 }
-Elephant.prototype.play = function () {
-    let i = Math.floor(Math.random()*this.tricks.length)
+Elephant.prototype.play = function() {
+    let i = Math.floor(Math.random() * this.tricks.length)
     console.log(`${this.name} is ${this.tricks[i]}!`)
 }
 let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
 let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
 let kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
 let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
-let herd = [ellie, charlie, kate, micah];
-Elephant.paradeHelper = function (elephant) {
+let herd = [ellie, charlie, kate, micah]
+Elephant.paradeHelper = function(elephant) {
     console.log(`${elephant.name} is trotting by`)
 }
 herd.forEach(Elephant.paradeHelper)
@@ -43,14 +47,14 @@ function dinerBreakfast() {
     let order = "I'd like cheesy scrambled eggs please."
     console.log(order)
     // return f(order, food)
-    return function (food) {
+    return function(food) {
         order = `${order.slice(0, order.length - 8)} and ${food} please.`
         console.log(order)
     }
 }
-let bfastOrder = dinerBreakfast();
-"I'd like cheesy scrambled eggs please"
-bfastOrder("chocolate chip pancakes");
-"I'd like cheesy scrambled eggs and chocolate chip pancakes please."
-bfastOrder("grits");
-"I'd like cheesy scrambled eggs and chocolate chip pancakes and grits please."
+let bfastOrder = dinerBreakfast()
+// "I'd like cheesy scrambled eggs please"
+console.log(bfastOrder("chocolate chip pancakes"))
+// "I'd like cheesy scrambled eggs and chocolate chip pancakes please."
+console.log(bfastOrder("grits"))
+// "I'd like cheesy scrambled eggs and chocolate chip pancakes and grits please."
