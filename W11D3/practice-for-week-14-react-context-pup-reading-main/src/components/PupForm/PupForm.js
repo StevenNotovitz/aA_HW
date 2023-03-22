26be3a7459fb688ecaafc3c2bd75b30321a86271
Steven Notovitz <sjn39@cornell.edu>
2023-03-22 10:01:45 -0400

    hw 1

--- W11D3/practice-for-week-14-react-context-pup-reading-main/src/components/PupForm/PupForm.js
+++ W11D3/practice-for-week-14-react-context-pup-reading-main/src/components/PupForm/PupForm.js
@@ -1,14 +1,17 @@
 import {useState} from 'react';
+import {usePuppyType} from '../../context/PupContext';
 import banana from '../../pups/banana-pup.jpg';
 import sleepy from '../../pups/sleepy-pup.jpg';
 import speedy from '../../pups/speedy-pup.jpg';
 
 function PupForm() {
-  const [selectedPup, setSelectedPup] = useState('select');
+  const {puppyType, setPuppyType} = usePuppyType();
+  const [selectedPup, setSelectedPup] = useState(puppyType);
 
   const onSubmit = (e) => {
     e.preventDefault();
-  }
+    setPuppyType(selectedPup);
+  };
 
   return (
     <form onSubmit={onSubmit}>
@@ -18,9 +21,9 @@ function PupForm() {
         value={selectedPup}
       >
         <option value="select" disabled>Select a pup!</option>
-        <option value={speedy}>Speedy Pup</option>
         <option value={banana}>Banana Pup</option>
         <option value={sleepy}>Sleepy Pup</option>
+        <option value={speedy}>Speedy Pup</option>
       </select>
       <button>
         Submit
--- W11D3/practice-for-week-14-react-context-pup-reading-main/src/context/PupContext.js
+++ W11D3/practice-for-week-14-react-context-pup-reading-main/src/context/PupContext.js
@@ -1,10 +1,12 @@
-import {createContext, useState, useContext} from 'react'
+import {createContext, useState, useContext} from 'react';
+import banana from '../pups/banana-pup.jpg';
+import sleepy from '../pups/sleepy-pup.jpg';
 import speedy from '../pups/speedy-pup.jpg';
 
 export const PupContext = createContext();
 
 export function PupProvider(props) {
-  const [puppyType, setPuppyType] = useState(speedy);
+  const [puppyType, setPuppyType] = useState("select");
 
   return (
     <PupContext.Provider value={{puppyType, setPuppyType}}>
